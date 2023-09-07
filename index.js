a750320686fba9a5c0ac745a34b08102b1174092
DavidC <david19cardenas@gmail.com>
2023-09-07 15:24:48 -0500

    Lista de contactos cambios de rama

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const rl = readline.createInterface({
 let listaDeTareas=[ ]; 
     function agregarTarea (tarea){
         listaDeTareas.push(tarea);
-        
     };
     function actualizarTarea(id, tareaAActualizar, nuevoValor) {
         const indice = listaDeTareas.findIndex(tarea => tarea.id === id);
@@ -29,7 +28,6 @@ let listaDeTareas=[ ];
                 console.log("Identificador de tarea:", tarea.id);
                 console.log("Descripción:", tarea.descripción);
                 console.log("Estado:", tarea.estado);
-            
             }
     };
     function preguntarOpcion() {
@@ -41,36 +39,28 @@ let listaDeTareas=[ ];
             "4. Mostrar tareas", 
             "5. Salir"
         ];
-        
     opciones.forEach((opcion, indice) => {
         console.log(`${indice + 1}. ${opcion}`);
     });
         rl.question('Por favor, elige una opción: ', (respuesta) => {
             // esperar respuesta del usuario 
             const eleccion = parseInt(respuesta);
-        
         if (eleccion  >= 1 && eleccion <= opciones.length) {
             if (eleccion === 1) {
-    
         rl.question("Ingrese el ID de la nueva tarea:", (id) => {
             rl.question("Descripcion de la nueva Tarea:", (descripción) => {
                 rl.question("Estado de la nueva tarea:", (estado) => {
-                
                     const nuevaTarea =  {
                         id: parseInt(id),
                         descripción,
                         estado  
-            
         };
-            
             agregarTarea(nuevaTarea);
             console.log("Tarea agregada exitosamente.");
             preguntarOpcion(); // Continuar con las preguntas  
             });
         });
     });
-            
-                
         } else if (eleccion == 2) {
             rl.question("Ingrese el ID de la tarea a actualizar", (idAActualizar ) => {
                     const tareaExistente = listaDeTareas.find((tarea) => tarea.id === parseInt(idAActualizar));
